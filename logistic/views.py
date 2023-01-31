069ecdc7f686034de59ddc7778cac746d85e1579
VadimZ92 <hemulyaa@gmail.com>
2023-01-31 23:21:55 +0500

    update11

--- logistic/views.py
+++ logistic/views.py
@@ -14,7 +14,7 @@ class ProductViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["title", ]
     search_fields = ['id', 'title', 'description']
-    ordering_fields = ['id', ] # Фильтр
+    ordering_fields = ['id', ]
 
     @action(['GET'], detail=False)
     def test(self, request):
